d1c4133325db621088b187fe3b96259f57f93a09
ernestognw <ernestognw@gmail.com>
2020-02-04 13:38:09 -0600

    chore(index.graphql): Add security directives to subscription
    Due to a DoS attack on subscription listeners, the jwt verification is needed, so there are no exceeded
    
    No breaking changes

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,27 +1,38 @@
-import path from 'path';
-import {ApolloServer, PubSub} from 'apollo-server';
-import {makeExecutableSchema} from 'graphql-tools';
-import {importSchema} from 'graphql-import';
-import schemaDirectives from './directives';
-import resolvers from './resolvers';
+import path from "path";
+import { ApolloServer, PubSub } from "apollo-server";
+import { makeExecutableSchema } from "graphql-tools";
+import { importSchema } from "graphql-import";
+import schemaDirectives from "./directives";
+import resolvers from "./resolvers";
 
-const typeDefs = importSchema(path.join(__dirname, './typedefs/index.graphql'));
+const typeDefs = importSchema(path.join(__dirname, "./typedefs/index.graphql"));
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-  schemaDirectives,
+  schemaDirectives
 });
 
 const pubsub = new PubSub();
 
+setInterval(() => {
+  console.log("NEW_MESSAGE: ", pubsub.ee.listenerCount("NEW_MESSAGE"));
+  console.log("NEW_POST: ", pubsub.ee.listenerCount("NEW_POST"));
+  console.log("NEW_TICKET: ", pubsub.ee.listenerCount("NEW_TICKET"));
+  console.log("TICKET_UPDATE: ", pubsub.ee.listenerCount("TICKET_UPDATE"));
+  console.log("ACTIVE_TICKETS: ", pubsub.ee.listenerCount("ACTIVE_TICKETS"));
+  console.log("TURN_UPDATE: ", pubsub.ee.listenerCount("TURN_UPDATE"));
+}, 1000);
+
 const server = new ApolloServer({
   schema,
-  context: ({req, res}) => ({req, res, pubsub}),
-  cors: {
-    origin: '*',
-    credentials: true,
+  context: ({ req, res }) => {
+    return { req, res, pubsub };
   },
+  cors: {
+    origin: "*",
+    credentials: true
+  }
 });
 
 export default server;
