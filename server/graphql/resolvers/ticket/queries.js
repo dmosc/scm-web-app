06cf7c23dc29b2f99e2823c0b7eb168d35bfb057
Oscar David Rodríguez Moscosa <a01229551@itesm.mx>
2020-02-13 02:05:12 -0600

    feature ( index.js ) Add content to History section
    Added a full table display to manage and show archived Tickets available at the AuroraDB. Table functionality, pagination and filtering is expected to increase as new features and requirements pop along. Added new archivedTickets and ArchiveTicket definitions in GraphQL to support different data content in contrast with a MongoDB Ticket object.
    
    No breaking changes.

--- server/graphql/resolvers/ticket/queries.js
+++ server/graphql/resolvers/ticket/queries.js
@@ -1,5 +1,7 @@
 import { ApolloError } from 'apollo-server';
+import { Op } from 'sequelize';
 import { Ticket } from '../../../mongo-db/models';
+import { Ticket as ArchiveTicket } from '../../../sequelize-db/models';
 import authenticated from '../../middleware/authenticated';
 
 const ticketQueries = {
@@ -26,6 +28,31 @@ const ticketQueries = {
 
     if (!activeTickets) throw new ApolloError('¡Ha habido un error cargando los tickets!');
     else return activeTickets;
+  }),
+  archivedTickets: authenticated(async (_, { filters: { limit, offset, search: oldSearch, start, end, date } }) => {
+    const search = `%${oldSearch}%`;
+
+    const archivedTickets = await ArchiveTicket.findAll({
+      limit: limit || 100,
+      offset: offset || 0,
+      where: {
+        [Op.or]: [
+          { createdAt: date },
+          { createdAt: { [Op.between]: [start, end] } },
+          { folio: { [Op.like]: search || '%' } },
+          { driver: { [Op.like]: search || '%' } },
+          { client: { [Op.like]: search || '%' } },
+          { businessName: { [Op.like]: search || '%' } },
+          { address: { [Op.like]: search || '%' } },
+          { rfc: { [Op.like]: search || '%' } },
+          { plates: { [Op.like]: search || '%' } },
+          { product: { [Op.like]: search || '%' } }
+        ]
+      }
+    });
+
+    if (!archivedTickets) throw new ApolloError('¡Ha habido un error cargando los tickets!');
+    else return archivedTickets;
   })
 };
 
