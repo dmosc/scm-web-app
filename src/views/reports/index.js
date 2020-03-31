463cee7922eb3e8b525a14c2f19756a47f7cc561
ernestognw <ernestognw@gmail.com>
2020-03-31 00:56:49 -0600

    feature(code-splitting): Add code splitting to reports
    Report subroutes needed code-splitting. It was implemented.
    
    No breaking changes

--- src/views/reports/index.js
+++ src/views/reports/index.js
@@ -2,14 +2,46 @@ import React, { useState } from 'react';
 import { withAuth } from 'components/providers/withAuth';
 import { useDebounce } from 'use-lodash-debounce';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import TopBarProgress from 'react-topbar-progress-indicator';
+import Loadable from 'react-loadable';
 import { ReportsContainer } from './elements';
 import TitleSection from './components/title-section';
-import Global from './components/global';
-import Products from './components/products';
-import Tickets from './components/tickets';
-import Clients from './components/clients';
-import Trucks from './components/trucks';
-import Turns from './components/turns';
+
+/* webpackChunkName: "Global" */
+const Global = Loadable({
+  loader: () => import('./components/global'),
+  loading: TopBarProgress
+});
+
+/* webpackChunkName: "Products" */
+const Products = Loadable({
+  loader: () => import('./components/products'),
+  loading: TopBarProgress
+});
+
+/* webpackChunkName: "Tickets" */
+const Tickets = Loadable({
+  loader: () => import('./components/tickets'),
+  loading: TopBarProgress
+});
+
+/* webpackChunkName: "Clients" */
+const Clients = Loadable({
+  loader: () => import('./components/clients'),
+  loading: TopBarProgress
+});
+
+/* webpackChunkName: "Trucks" */
+const Trucks = Loadable({
+  loader: () => import('./components/trucks'),
+  loading: TopBarProgress
+});
+
+/* webpackChunkName: "Turns" */
+const Turns = Loadable({
+  loader: () => import('./components/turns'),
+  loading: TopBarProgress
+});
 
 const Reports = () => {
   const [globalFilters, setGlobalFilters] = useState({
